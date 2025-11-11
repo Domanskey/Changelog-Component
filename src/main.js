31eb8ff1fddbf45c2bddbdf958a1c2206fcfa441
domanskey <domadam04@gmail.com>
2025-11-11 18:41:48 +0100

    add dots transition and left start

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import './style.css'
 
-const items = document.querySelectorAll('.item-description');
+const items = document.querySelectorAll('.list__item');
 
 for (let i = 0; i < items.length; i++) {
   setTimeout(() => {
