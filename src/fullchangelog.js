2489f2162c52c6f895d45cb928d2d218ce118a0a
domanskey <domadam04@gmail.com>
2025-11-17 22:19:34 +0100

    add full changelog styling and js

--- src/fullchangelog.js
+++ src/fullchangelog.js
@@ -1,9 +1,44 @@
-import './style.css'
+import './fullchangelog.css'
 
-const items = document.querySelectorAll('.item-description');
+const targets = document.querySelectorAll(".list__item ");
+const threshold = 0.5;
+const ANIMATED_CLASS = "visible";
+const STAGGER_INCREMENT = 400;
+const RESET_TIMEOUT = 500;
 
-for (let i = 0; i < items.length; i++) {
-  setTimeout(() => {
-    items[i].classList.add('visible');
-  }, i * 100); // Delay each item's appearance by 100ms
+
+let staggerDelay = 0;
+let delayResetTimer = null;
+
+
+function callback(entries, observer) {
+
+  clearTimeout(delayResetTimer);
+
+  entries.forEach((entry) => {
+
+    const elem = entry.target;
+
+    if (entry.intersectionRatio >= threshold) {
+
+      setTimeout(() => {
+        elem.classList.add(ANIMATED_CLASS);
+      }, staggerDelay);
+
+      staggerDelay += STAGGER_INCREMENT;
+
+      observer.unobserve(elem);
+    } else {
+      elem.classList.remove(ANIMATED_CLASS);
+    }
+  });
+
+  delayResetTimer = setTimeout(() => {
+    staggerDelay = 0;
+  }, RESET_TIMEOUT);
+}
+
+const observer = new IntersectionObserver(callback, { threshold });
+for (const target of targets) {
+  observer.observe(target);
 }
